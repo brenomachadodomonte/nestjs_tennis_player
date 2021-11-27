aeca430a856dbccb31b5e8e926af664f91ca3ccf
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-11-27 19:13:00 -0300

    Creating valition when creating Player

--- src/player/dto/create-player.dto.ts
+++ src/player/dto/create-player.dto.ts
@@ -1,7 +1,14 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 
 export class CreatePlayerDto {
+    
+    @IsNotEmpty()
     cellphone: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     name: string;
 }
