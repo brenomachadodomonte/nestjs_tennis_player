cc27b73b9edc028afe4f2ff6791acfba28e610e9
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-12-01 14:30:00 -0300

    Creating Category Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { PlayerModule } from './player/player.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     PlayerModule,
-    MongooseModule.forRoot('mongodb://localhost:27017')
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    CategoryModule
   ],
   controllers: [],
   providers: [],
