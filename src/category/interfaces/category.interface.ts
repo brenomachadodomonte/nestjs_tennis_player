52b13d422c55373c28d3d6551700d35b225902ed
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-12-03 14:30:00 -0300

    Creating event interface and changing category interface

--- src/category/interfaces/category.interface.ts
+++ src/category/interfaces/category.interface.ts
@@ -1,6 +1,12 @@
 import { Document } from "mongoose";
+import { Event } from "./event.interface";
 
 
 export interface Category extends Document {
-    
+
+    readonly category: string;
+    description: string;
+    events: Event[];
+
 }
+
