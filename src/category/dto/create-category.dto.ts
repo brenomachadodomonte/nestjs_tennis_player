0439d1aa222f3872102471b9b841d4a8e1bf7843
brenomachadodomonte <brenomachadodomonte@gmail.com>
2021-12-05 14:30:00 -0300

    Adding valition to create category dto

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,8 +1,16 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from "class-validator";
 
 export class createCategoryDto {
     
+    @IsString()
+    @IsNotEmpty()
     readonly category: string;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 
+    @IsArray()
+    @ArrayMinSize(1)
     events: Event[];
 }
